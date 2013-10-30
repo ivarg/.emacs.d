41cfe03b7246707ba9803e330d20c4931bd28b1f
Ivar Gaitan <ivar.gaitan@gmail.com>
2013-10-30 21:55:03 +0100

    added go-mode as a submodule

--- setup-go-mode.el
+++ setup-go-mode.el
@@ -2,15 +2,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Go programming language stuff
 
-(require 'go-mode-load)
+(add-to-list 'load-path (concat user-emacs-directory "packages/go-mode"))
+(require 'go-mode)
 (require 'auto-complete-config)
 (require 'go-autocomplete)
 (global-auto-complete-mode)
 
 (add-hook 'go-mode-hook (lambda()
-                          (setq tab-width 4)
                           (local-set-key (kbd "C-j") 'go-mode-insert-and-indent)
-                          (local-set-key (kbd "C-c C-a") 'go-imports-add)
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
                           (local-set-key (kbd "C-c C-i") 'go-goto-imports)
                           (smartparens-mode)
