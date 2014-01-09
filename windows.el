b980f0998d533d3b81e2503292d48afacfc70cda
Ivar Gaitan <ivar.gaitan@sungard.com>
2014-01-09 13:35:26 +0100

    start emacs server when running on Windows

--- windows.el
+++ windows.el
@@ -3,5 +3,7 @@
 
 ;; Set default font and size
 (set-face-attribute 'default nil :font "Consolas-12.0" :height 110)
-
+(load "server")
+(unless (server-running-p)
+  (server-start))
 (provide 'windows)
