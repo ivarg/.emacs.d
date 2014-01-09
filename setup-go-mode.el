8529d4f0f193757b8e09effc2959c0cfb86c426a
Ivar Gaitan <ivar.gaitan@gmail.com>
2014-01-09 09:40:10 +0100

    use go-mode from the Go distribution
    also, include goflymake and gocode from github.com

--- setup-go-mode.el
+++ setup-go-mode.el
@@ -2,21 +2,32 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Go programming language stuff
 
-(add-to-list 'load-path (concat user-emacs-directory "packages/go-mode"))
-(require 'go-mode)
-(require 'auto-complete-config)
+;; (add-to-list 'load-path (concat user-emacs-directory "packages/go-mode"))
+(add-to-list 'load-path (concat (getenv "GOROOT") "/libexec/misc/emacs"))
+(add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/nsf/gocode/emacs"))
+(add-to-list 'load-path (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake"))
+
+(require 'go-mode-load)
+
 (require 'go-autocomplete)
+(require 'auto-complete-config)
+
+(require 'go-flymake)
+(require 'go-flycheck)
+
 (global-auto-complete-mode)
 
+(add-hook 'before-save-hook 'gofmt-before-save)
+
 (add-hook 'go-mode-hook (lambda()
-                          (local-set-key (kbd "C-j") 'go-mode-insert-and-indent)
+                          ;; (local-set-key (kbd "C-j") 'go-mode-insert-and-indent)
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
                           (local-set-key (kbd "C-c C-i") 'go-goto-imports)
-                          (smartparens-mode)
+                          (local-set-key (kbd "M-.") 'godef-jump)
+                          ;; (smartparens-mode)
                           ))
 
 
-(add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; END Go programming language stuff
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
