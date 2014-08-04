76e21ba9a8afbed79ab9793a96871cff1b3fbde7
Ivar Gaitan <ivar.gaitan@sungard.com>
2014-08-04 13:27:56 +0200

    Fetch cmake-mode from github.com/roysc/cmake-mode

--- setup-cmake-mode.el
+++ setup-cmake-mode.el
@@ -1,19 +1,14 @@
 
 ;; Download cmake-mode.el
-(setq cmake-dir (concat user-emacs-directory "/packages/cmake"))
-(unless (file-exists-p cmake-dir)
-  (make-directory cmake-dir))
-(unless (file-exists-p (concat cmake-dir "/cmake-mode.el"))
-  (url-copy-file "http://www.cmake.org/CMakeDocs/cmake-mode.el"
-                 (concat cmake-dir "/cmake-mode.el") t))
+(setq cmake-dir (concat user-emacs-directory "/packages/cmake-mode"))
 
 (unless (file-exists-p (concat cmake-dir "/andersl-cmake-font-lock.el"))
   (url-copy-file "https://raw.githubusercontent.com/Lindydancer/cmake-font-lock/master/andersl-cmake-font-lock.el"
                  (concat cmake-dir "/andersl-cmake-font-lock.el") t))
 
-(add-to-list 'load-path (concat user-emacs-directory "packages/cmake"))
+(add-to-list 'load-path cmake-dir)
+(require 'cmake-mode)
 
-(autoload 'cmake-mode "cmake-mode" "CMake Mode" t)
 (autoload 'andersl-cmake-font-lock-activate "andersl-cmake-font-lock" nil t)
 (add-hook 'cmake-mode-hook 'andersl-cmake-font-lock-activate)
 
@@ -23,8 +18,5 @@
 	   '(("CMakeLists\\.txt\\'" . cmake-mode))
 	   '(("\\.cmake\\'" . cmake-mode))
 	   auto-mode-alist))
-
-;; (autoload 'cmake-mode "~/CMake/cmake-mode.el" t)
-
 
 (provide 'setup-cmake-mode)
