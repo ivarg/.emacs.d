ca166636bb7ae096080f95f6e76684aaecb69ac5
Ivar Gaitan <ivar.gaitan@gmail.com>
2014-11-26 08:23:04 +0100

    move separate font-lock file out of cmake-mode package dir

--- setup-cmake-mode.el
+++ setup-cmake-mode.el
@@ -2,9 +2,9 @@
 ;; Download cmake-mode.el
 (setq cmake-dir (concat user-emacs-directory "/packages/cmake-mode"))
 
-(unless (file-exists-p (concat cmake-dir "/andersl-cmake-font-lock.el"))
+(unless (file-exists-p (concat user-emacs-directory "/andersl-cmake-font-lock.el"))
   (url-copy-file "https://raw.githubusercontent.com/Lindydancer/cmake-font-lock/master/andersl-cmake-font-lock.el"
-                 (concat cmake-dir "/andersl-cmake-font-lock.el") t))
+                 (concat user-emacs-directory "/andersl-cmake-font-lock.el") t))
 
 (add-to-list 'load-path cmake-dir)
 (require 'cmake-mode)
